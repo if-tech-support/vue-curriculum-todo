a9b2b7ed40b4128e036d172e295b004def191fef
Iori <iki.16b@gmail.com>
2021-01-26 20:39:17 +0900

    add: closed#4 Update機能完了

--- app.js
+++ app.js
@@ -5,7 +5,13 @@ const app = new Vue({
     todo: {
       id: 0,
       title: null
-    }
+    },
+    update: {
+      id: null,
+      title: null
+    },
+    onUpdate: false
+
   },
   methods: {
     addTodo() {
@@ -14,9 +20,20 @@ const app = new Vue({
         title: this.todo.title
       })
       this.todo.id++
+      this.todo.title = null
     },
     removeTodo(i) {
       this.todos.splice(i, 1)
+    },
+    getTodo(todo, i) {
+      this.onUpdate = !this.onUpdate
+      this.update.title = todo.title
+      this.update.id = i
+    },
+    updateTodo() {
+      this.onUpdate = !this.onUpdate
+      let id = this.update.id
+      this.todos[id].title = this.update.title
     }
   },
 
